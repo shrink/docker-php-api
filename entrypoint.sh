10231e746058d59dbb1590f72f1576f4014f8356
Samuel Ryan <sam@samryan.co.uk>
2020-10-27 12:34:31 +0000

    :recycle: Reduce frequency of php-fpm startup check

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 php-fpm -g /tmp/php-fpm.pid -F &
 
-until [ -f /tmp/php-fpm.pid ]; do usleep 50000; done
+until [ -f /tmp/php-fpm.pid ]; do usleep 10000; done
 
 nginx -g 'daemon off;' &
 
